50471e5fdc1dc0fb73aa7cc51080560cd815befa
Lucas De Angelis <deangelis.lucas@outlook.com>
2020-12-12 19:54:39 +0100

    Day 12, part 2 and cleaning up done

--- day12/solution.js
+++ day12/solution.js
@@ -1,14 +1,39 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
-
-const input = require("fs").readFileSync("input.txt", "utf-8");
-const inputTest = require("fs").readFileSync("inputTest.txt", "utf-8");
-
-console.log(inputTest);
+const assert = require("assert");
+const input = parse(require("fs").readFileSync("input.txt", "utf-8"));
+const inputTest = parse(require("fs").readFileSync("inputTest.txt", "utf-8"));
 
 function parse(puzzleInput) {
   return puzzleInput.split("\n").map((el) => {
     return { action: el[0], value: parseInt(el.slice(1)) };
   });
+}
+
+assert.strictEqual(part1(input), 439, "Part 1 fails");
+assert.strictEqual(part2(input), 12385, "Part 2 fails");
+console.log("All tests pass");
+
+function part1(actions) {
+  let position = { north: 0, east: 0 };
+  let orientation = "E";
+
+  for (const action of actions) {
+    switch (action.action) {
+      case "F":
+        position = move(position, orientation, action.value);
+        break;
+      case "E":
+      case "W":
+      case "N":
+      case "S":
+        position = move(position, action.action, action.value);
+        break;
+      case "L":
+      case "R":
+        orientation = newOrientation(orientation, action.action, action.value);
+        break;
+    }
+  }
+  return Math.abs(position.north) + Math.abs(position.east);
 }
 
 function move(currentPosition, orientation, value) {
@@ -44,6 +69,16 @@ function move(currentPosition, orientation, value) {
   return position;
 }
 
+function newOrientation(oldOrientation, moveDirection, degrees) {
+  let num = 0;
+  if (moveDirection == "R") {
+    num = modulo(orientationToNum(oldOrientation) + degrees / 90, 4);
+  } else if (moveDirection == "L") {
+    num = modulo(orientationToNum(oldOrientation) - degrees / 90, 4);
+  }
+  return numToOrientation(num);
+}
+
 function orientationToNum(orientation) {
   switch (orientation) {
     case "N":
@@ -55,6 +90,10 @@ function orientationToNum(orientation) {
     case "W":
       return 3;
   }
+}
+
+function modulo(a, n) {
+  return ((a % n) + n) % n;
 }
 
 function numToOrientation(num) {
@@ -70,52 +109,61 @@ function numToOrientation(num) {
   }
 }
 
-function modulo(a, n) {
-  return ((a % n) + n) % n;
-}
-
-function newOrientation(oldOrientation, moveDirection, degrees) {
-  let num = 0;
-  if (moveDirection == "R") {
-    num = modulo(orientationToNum(oldOrientation) + degrees / 90, 4);
-  } else if (moveDirection == "L") {
-    num = modulo(orientationToNum(oldOrientation) - degrees / 90, 4);
-  }
-  return numToOrientation(num);
-}
-
-console.log(newOrientation("N", "R", 0));
-console.log(newOrientation("N", "R", 90));
-console.log(newOrientation("N", "R", 180));
-console.log(newOrientation("N", "R", 270));
-console.log(newOrientation("N", "L", 0));
-console.log(newOrientation("N", "L", 90));
-console.log(newOrientation("N", "L", 180));
-console.log(newOrientation("N", "L", 270));
-
-function moveShip(actions) {
+function part2(actions) {
   let position = { north: 0, east: 0 };
+  let waypoint = { north: 1, east: 10 };
   let orientation = "E";
 
   for (const action of actions) {
     switch (action.action) {
       case "F":
-        position = move(position, orientation, action.value);
+        position = moveWithWaypoint(position, waypoint, action.value);
         break;
       case "E":
       case "W":
       case "N":
       case "S":
-        position = move(position, action.action, action.value);
+        waypoint = move(waypoint, action.action, action.value);
         break;
       case "L":
       case "R":
-        orientation = newOrientation(orientation, action.action, action.value);
+        waypoint = moveWaypoint(
+          orientation,
+          action.action,
+          action.value,
+          waypoint
+        );
         break;
     }
   }
-
   return Math.abs(position.north) + Math.abs(position.east);
 }
 
-console.log(moveShip(parse(input)));
+function moveWithWaypoint(position, waypoint, times) {
+  return {
+    north: position.north + times * waypoint.north,
+    east: position.east + times * waypoint.east,
+  };
+}
+
+function moveWaypoint(orientation, moveDirection, degrees, waypoint) {
+  const newOri = newOrientation(orientation, moveDirection, degrees);
+
+  switch (distanceOrientation(orientation, newOri)) {
+    case 0:
+      return waypoint;
+    case 1:
+      return { north: -waypoint.east, east: waypoint.north };
+    case 2:
+      return { north: -waypoint.north, east: -waypoint.east };
+    case 3:
+      return { north: waypoint.east, east: -waypoint.north };
+  }
+}
+
+function distanceOrientation(orientationA, orientationB) {
+  return modulo(
+    orientationToNum(orientationB) - orientationToNum(orientationA),
+    4
+  );
+}
